121c949385c6687aabf9066a428f1004a581f1b1
edidiongapostle <edidiongapostle@gmail.com>
2023-02-16 10:14:04 +0100

    still a test

--- fbank/README.md
+++
@@ -1,1 +0,0 @@
-This a test to see how to copy files from my computer to github
--- fbank/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['admin'], $_SESSION['wire_transfer'],$_SESSION['dom-transfer'],$_SESSION['login']);
-setcookie('firstVisit');
-header("Location:./login.php");
